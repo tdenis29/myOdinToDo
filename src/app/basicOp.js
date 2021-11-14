5c12e403de6dfaa7444f8fc395bc92414f41fc34
tdenis29 <tdenis29@outlook.com>
2021-11-13 16:49:50 -0800

    okdokie

---
+++ src/app/basicOp.js
@@ -0,0 +1,30 @@
+const basicOp = (() => {
+// Project modal
+//open modal
+const addNewProjectForm = document.getElementById('addNewProjectForm');
+const projectOverlay = document.getElementById('projectOverlay')
+const projectmodalClose = document.getElementById("projectmodalClose");
+
+addNewProjectForm.addEventListener('click', (e) => {
+    projectOverlay.style.display = 'block';
+})
+
+projectmodalClose.addEventListener('click', () => {
+    projectOverlay.style.display = "none"
+});
+
+// ToDo Modal 
+const addNewTodoForm = document.getElementById('addNewTodoForm');
+const todoOverlay = document.getElementById('todoOverlay')
+const todomodalClose = document.getElementById("todomodalClose");
+
+addNewTodoForm.addEventListener('click', (e) => {
+    todoOverlay.style.display = 'block';
+})
+
+todomodalClose.addEventListener('click', () => {
+    todoOverlay.style.display = "none"
+    });
+
+
+})();
